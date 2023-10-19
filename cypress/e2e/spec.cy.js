ef9592a76c4284de72af3a732b1a94622500a965
Lewis Brennan <lewis.brennan@2itesting.com>
2023-10-19 16:00:21 +0100

    third commit, fixed asynch variable reassigning bug

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -20,35 +20,63 @@ describe('monolith', () => {
     cy.get('#modal-save-button').click()
     cy.get('#modal-ok-button').click()
   });
+  // it('Can use a template to generate data', () => {
+  //   let interceptedFileName = 'notAssigned';
+  //   cy.get('a.nav-links[href="/data"]').click()
+  //   cy.get('#templates-selector').select(5)
+  //   cy.get('#submit-template').click()
+  //   cy.get('#csv-json-btn').click() 
+  //   cy.get('#generate-values').click()
+  //   cy.log(interceptedFileName);
+  //   cy.get('#file-name-input').invoke('val').then((dataName) => {
+  //     cy.log(dataName);
+  // });
+  //   cy.get('#file-name-input').invoke('val').then((dataName) =>  {
+  //     interceptedFileName = dataName;
+  //     cy.log(interceptedFileName); 
+  //   });
+  //   cy.log(interceptedFileName);
+  //   cy.get('#upload-button').click()
+  //   cy.get('#modal-ok-button').click()
+  //   cy.get('a.nav-links[href="/"]').click()
+  //   cy.get('a.nav-links[href="/data"]').click()
+  //   cy.get('#templates-selector').select('test1').should('contain', 'test1')
+  //   cy.get('a.nav-links[href="/"]').click()
+  //   cy.get('a.nav-links[href="/data"]').click()
+  //   cy.get('#templates-selector').select(5)
+  //   cy.get('#delete-template').click()
+  //   cy.get('#confirm-modal-confirm').click()
+  //   cy.get('#modal-ok-button').click()
+  // })
   it('Can use a template to generate data', () => {
-    let HSLBTDGfileName = 'notAssigned';
     cy.get('a.nav-links[href="/data"]').click()
-    cy.get('#templates-selector').select(5)
-    cy.get('#submit-template').click()
-    cy.get('#csv-json-btn').click() 
-    cy.get('#generate-values').click()
-    cy.log(HSLBTDGfileName);
-    cy.get('#file-name-input').invoke('val').then((value) => {
-      cy.log(value);
-  });
-    cy.get('#file-name-input').invoke('val').then((dataName) =>  {
-      expect(dataName).to.not.be.empty;
-      HSLBTDGfileName = dataName; 
+    .get('#templates-selector').select(5)
+    .get('#submit-template').click()
+    .get('#csv-json-btn').click() 
+    .get('#generate-values').click()
+    .get('#file-name-input').invoke('val').then((dataName) => {
+      cy.log(dataName)
+      .get('#file-name-input')  // We're chaining the commands here.
+      .invoke('val').then((valueFromInput) => {
+        const interceptedFileName = valueFromInput; 
+        cy.log(interceptedFileName)
+        .get('#upload-button').click()
+        .get('#modal-ok-button').click()
+        .get('a.nav-links[href="/"]').click()
+        .get('a.nav-links[href="/data"]').click()
+        .get('#templates-selector').select('test1').should('contain', 'test1')
+        .get('a.nav-links[href="/"]').click()
+        .get('a.nav-links[href="/history"]').click()
+        .get('a.nav-links[href="/"]').click()
+        .get('a.nav-links[href="/data"]').click()
+        .get('#templates-selector').select(5)
+        .get('#delete-template').click()
+        .get('#confirm-modal-confirm').click()
+        .get('#modal-ok-button').click();
+        cy.log(interceptedFileName);
+      });
     });
-    cy.log(HSLBTDGfileName);
-    cy.get('#upload-button').click()
-    cy.get('#modal-ok-button').click()
-    cy.get('a.nav-links[href="/"]').click()
-    cy.get('a.nav-links[href="/data"]').click()
-    cy.get('#templates-selector').select('test1').should('contain', 'test1')
-    cy.get('a.nav-links[href="/"]').click()
-    cy.get('a.nav-links[href="/data"]').click()
-    cy.get('#templates-selector').select(5)
-    cy.get('#delete-template').click()
-    cy.get('#confirm-modal-confirm').click()
-    cy.get('#modal-ok-button').click()
-    cy.log(HSLBTDGfileName);
-  })
+});
   // after(() => {
   //   cy.get('#logout-link').click()
   // });
